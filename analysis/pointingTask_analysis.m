e15632d01148c48a4ff0334e364a886cd2fe1921
Alex <alexandra.mitchell@ed.ac.uk>
2019-07-25 10:43:02 +0100

    Pointing task analysis
    Making analysis script to analyse pilot data collected week of 22.07

---
+++ analysis/pointingTask_analysis.m
@@ -0,0 +1,12 @@
+%% AG. Mitchell - 25.07.19
+%% DMT2019 Pointing task analysis script
+% Takes pointing data from .csv files extracted from opensesame experiment
+% Experiment: peripheral pointing task, touch targets presented at
+% 24,31,37deg eccentricity
+% Subtracts touch response to target from actual target location
+% Does this for each target location (3 trials per target loc), for each
+% eccentricity (9 trials per) and for each side (L/R - 27 trials per)
+
+% Task will be used to compare pointing in AD patients with control
+% patients
+% For now- use for healthy pilot data
