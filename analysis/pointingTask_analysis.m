4e6013a6576294870b8df1c3c1d6e7c70debb469
Alex <alexandra.mitchell@ed.ac.uk>
2019-07-25 12:36:47 +0100

    Pointing task analysis
    Trying to get data file import correct
    Getting there...

--- analysis/pointingTask_analysis.m
+++ analysis/pointingTask_analysis.m
@@ -10,3 +10,37 @@
 % Task will be used to compare pointing in AD patients with control
 % patients
 % For now- use for healthy pilot data
+
+%% Data files and variables
+dataPath = 'M:\Alex_Files\Experiments\DMT2019\DMT2019_rawData';
+anaPath = 'M:\Alex_Files\Experiments\DMT2019\dataAnalysis';
+nParticipants = 1:4; %number of participants
+% task 1 = closed loop norm, task 2 = closed loop fix, task 3 = closed loop
+% beep, task 4 = open loop
+nTasks = 1:4; %number of tasks used during testing
+
+allData = struct; %data structure
+
+%% Extracting data
+cd(dataPath) %to where the data is
+for p = 1:length(nParticipants)
+    ppID = sprintf('hp%0*d',2,nParticipants(p)); %for use when navigating files
+    anaFileName = sprintf('%s_pointingAnalysis.mat',ppID); %name of final analysis file
+    
+    dirData = [dataPath filesep ppID]; %where to extract data
+    % Data file names for each task
+    for t = 1:length(nTasks)
+        switch t
+            case 1
+                dataFileName_left = sprintf('subject90%d_pointingTask_CLnorm_left*', t);
+            case 2
+            case 3
+            case 4
+        end
+    end
+    % making analysis folder for data extraction
+    dirAna = mkdir([anaPath filesep ppID]); 
+   
+    % importing relevant filenames
+    
+end
