37d856566850d0f9afdcc577e4bcdbae551aaace
Alex <alexandra.mitchell@ed.ac.uk>
2019-07-26 13:02:09 +0100

    Pointing task analysis
    Calculating error - working out pixels per cm/deg and calculating error for each task
    Still working through this - need to figure out sign on screen

--- analysis/pointingTask_analysis.m
+++ analysis/pointingTask_analysis.m
@@ -19,6 +19,21 @@ nParticipants = 2; %for testing
 % task 1 = closed loop beep, task 2 = closed loop fix, task 3 = closed loop
 % norm, task 4 = open loop
 nTasks = 1:4; %number of tasks used during testing
+% screen information
+% getting pixels per cm/deg
+sd = 40;
+xRes = 1960; yRes = 1080;
+x = 31; y = 17.5;
+pix_perDeg = 43.76;
+deg_perPix = 1/pix_perDeg;
+
+% caluclating cm per pixel
+pixPer_cm_x = xRes/x;
+pixPer_cm_y = yRes/y;
+pixPer_cm = (pixPer_cm_x+pixPer_cm_y)/2;
+cm_perPix = 1/pixPer_cm;
+
+%deg_perPix = atan2(cm_perPix/(2*sd));
 
 allData = struct; %data structure
 
@@ -136,6 +151,34 @@ for p = 1:length(nParticipants)
                 rightMat = [trial, target_location, touch_response];
                 allData.(sprintf('%s', ppID)).(sprintf('%s', taskName)).rightMat = rightMat;
         end
+        
+        %% analysing the data
+        % getting error relative to target midpoint
+        % CL beep, left hand data
+        leftx_error = allData.(sprintf('%s', ppID)).CLbeep.leftMat(:,2) - ...
+            allData.(sprintf('%s', ppID)).CLbeep.leftMat(:,4); 
+        leftx_error_cm = leftx_error*cm_perPix; %converting from pix to cm
+        leftx_error_deg = leftx_error*deg_perPix; %converting from pix to deg
+        lefty_error = allData.(sprintf('%s', ppID)).CLbeep.leftMat(:,5) - ...
+            allData.(sprintf('%s', ppID)).CLbeep.leftMat(:,3); 
+        lefty_error_cm = lefty_error*cm_perPix;
+        lefty_error_deg = lefty_error*deg_perPix; %converting from pix to deg
+        
+        % right hand data
+        rightx_error = allData.(sprintf('%s', ppID)).CLbeep.rightMat(:,4) - ...
+            allData.(sprintf('%s', ppID)).CLbeep.rightMat(:,2); 
+        rightx_error_cm = rightx_error*cm_perPix; %converting from pix to cm
+        rightx_error_deg = rightx_error*deg_perPix; %converting from pix to deg
+        righty_error = allData.(sprintf('%s', ppID)).CLbeep.rightMat(:,5) - ...
+            allData.(sprintf('%s', ppID)).CLbeep.rightMat(:,3); 
+        righty_error_cm = righty_error*cm_perPix;
+        righty_error_deg = righty_error*deg_perPix; %converting from pix to deg
+        
+        % saving to matrices in allData
+        allData.(sprintf('%s', ppID)).CLbeep.leftError(:,1) = leftx_error_deg;
+        allData.(sprintf('%s', ppID)).CLbeep.leftError(:,2) = lefty_error_deg;
+        allData.(sprintf('%s', ppID)).CLbeep.rightError(:,1) = rightx_error_deg;
+        allData.(sprintf('%s', ppID)).CLbeep.rightError(:,2) = righty_error_deg;
         
         
     end
