6240978aa563d32777c149af175ef5304dc49d80
amitchell12 <agm26@st-andrews.ac.uk>
2019-08-14 21:09:51 +0100

    Pointing analysis
    Trying to make a pretty plot - legend and axis labels :)

--- analysis/CLFIX_pointing_analysis.R
+++ analysis/CLFIX_pointing_analysis.R
@@ -1,12 +1,13 @@
 library(readr)
 library(ggplot2)
 library(Rmisc)
-mypath
+
 #MANUALLY SET WORKING DIRECTORY TO DATA DIRECTORY AND LIST *.DAT CONTENTS
-mypath <- getwd()
+dataPath ="/Users/alexandramitchell/Documents/EDB_PostDoc/DMT2019/DMT2019_analysis/pointing"
+setwd(dataPath) #setting the path
 
 #make complete list of eyelink RESULTS files (trial information)
-resfiles=list.files(path=mypath, full.names=TRUE, recursive = FALSE, include.dirs = TRUE, pattern = "*.csv")
+resfiles=list.files(path=dataPath, full.names=TRUE, recursive = FALSE, include.dirs = TRUE, pattern = "*.csv")
 
 #make empty dataframe to stack up trial information
 CLF <- read.csv(text = "avg_rt,count_committed,count_valid_trial,datetime,eye_move,fix_x,land_x,land_y,reach_duration,response_time_target_mouse_response,sanity_check,subject_nr,subject_parity,targ_x,targ_y,target_onset,targets_file,time_touch_offset,touch_x")
@@ -15,7 +16,7 @@ CLF <- read.csv(text = "avg_rt,count_committed,count_valid_trial,datetime,eye_mo
 #stack them all up
 for(x in resfiles){
   tmp <- read.csv(x)
-  tmp$subject_nr <- as.numeric(substr(x, 44, 46))
+  #tmp$subject_nr <- as.numeric(substr(x))
   CLF <- rbind(CLF, tmp)
 }
 
@@ -25,21 +26,33 @@ CLF[CLF$subject_nr %in% c(801,804), "GRP"] <- "MCI"
 CLF[CLF$subject_nr %in% c(802,803), "GRP"] <- "AD"
 CLF$SUB <- factor(CLF$subject_nr)
 
-CLF$x_Err <- CLF$land_x-CLF$targ_x
+CLF$x_Err <- CLF$land_x-CLF$targ_x #x and y error
 CLF$y_Err <- CLF$land_y-CLF$targ_y
+#converting to mm
+pix_permm = 6.18249
+CLF$x_Err = CLF$x_Err/pix_permm
+CLF$y_Err = CLF$y_Err/pix_permm
 
-CLF[is.na(CLF$eye_move), "eye_move"] <- 0
+CLF[is.na(CLF$eye_move), "eye_move"] <- 0 #any 'na' on eye-move = 0
 
 CLF$ECC <- factor(cut(CLF$targ_x, 3, label=c("N", "M", "F")))
 CLF <- CLF[CLF$eye_move == 0, ]
 
-CLF$ABS_ERR <- sqrt(CLF$x_Err^2 + CLF$y_Err^2)
+CLF$ABS_ERR <- sqrt(CLF$x_Err^2 + CLF$y_Err^2) #absolute error calc
 
+#plotting individual participant data
+ggplot(CLF, aes(x=SUB, y=ABS_ERR, colour = ECC)) +
+  geom_boxplot(outlier.alpha=0) +
+  geom_jitter(position=position_dodge(width=.8), size=4, alpha=.5) +
+  ylim(0,50) + geom_hline(yintercept=10, linetype="dotted") +
+  labs(x = 'Group', y = 'Absolute error (mm)') + 
+  theme_bw() + theme(legend.position = "bottom") -> ppPlot
+ppPlot
 
 ggplot(CLF, aes(x=SUB, y=ABS_ERR, colour=ECC)) +
   geom_boxplot(outlier.alpha=0) +
   geom_jitter(position=position_dodge(width=.8), size=4, alpha=.5) +
-  ylim(0,300) + geom_hline(yintercept=55, linetype="dotted")+
+  ylim(0,50) + geom_hline(yintercept=10, linetype="dotted")+
   theme_bw() + 
 
 
