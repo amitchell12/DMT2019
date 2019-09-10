548bbc66cef8054f9773a07f0a0d55a24ae8019f
Alex <alexandra.mitchell@ed.ac.uk>
2019-09-10 10:42:28 +0100

    Libtva analysis script - matlab
    Creating an analysis pipeline in matlab for easy running

---
+++ analysis/TVA/modellingTVA.m
@@ -0,0 +1,9 @@
+%% A.G. Mitchell - 10.09.19
+% This script takes TVA data collected for Dunhill Medical Trust funded
+% study investigating visual attention and visual motor mechanisms in AD
+% (McIntosh, Rossit, Mitchell, Pal & Hornberger)
+% Extracts data from the TVA paradigm (R formatted scripts) and models each
+% individual participant's data to calculated VSTM capacity (C) and processing
+% speed (k)
+
+%% Extracting data
