7315858b2b25b33362604f32cb3d228c3da31a0c
Alex <alexandra.mitchell@ed.ac.uk>
2019-09-25 13:17:35 +0100

    Editing TVA analysis code
    Formatting so it can batch analyse
    Creating 2 plots (1 by condition, 1 by exposure duration)

--- analysis/TVA/modellingTVA.m
+++ analysis/TVA/modellingTVA.m
@@ -7,28 +7,42 @@
 % speed (k)
 
 %% Extracting data
-dataDir = 'M:\Alex_Files\Experiments\DMT2019\dataAnalysis\TVA';
+dataDir = 'M:\Alex_Files\Experiments\DMT2019\DMT2019_rawdata\formatted_TVA';
 cd(dataDir)
 files = dir(fullfile(dataDir, 'subject*.dat'));
 
 %% TVA file prep
-filecheck = tvacheckdatafile(files.name);
-% load data
-% need to include the 'STD' [5 6] for masked conditions
-tvadata = tvaloader(files.name, 'STD', [5 6]);
-% tva report on data
-tvareport(tvadata) %leave open to view
-
-%% Fitting model
-[theta, tvamodel, tvadata, df] = tvafit(tvadata);
-%see the fitted values
-tvareport(tvadata, tvamodel, theta);
-
-%% Plotting
-[tt, oo, pp, cc] = tvaplot(tvadata, tvamodel, theta);
-figure()
-plot(tt,pp,'*')
-ylabel('Mean score'); xlabel('Exposure duration (ms)');
-xlim([0 250]); 
+for i = 1:length(files)
+    participantID = files(i).name(1:10);
+    filecheck = tvacheckdatafile(files(i).name)
+    % load data
+    % need to include the 'STD' [5 6] for masked conditions
+    tvadata = tvaloader(files(i).name, 'STD', [5 6]);
+    % tva report on data
+    tvareport(tvadata) %leave open to view
+    
+    %% Fitting model
+    [theta, tvamodel, tvadata, df] = tvafit(tvadata);
+    %see the fitted values
+    tvareport(tvadata, tvamodel, theta);
+    
+    %% Plotting
+    [tt, oo, pp, cc] = tvaplot(tvadata, tvamodel, theta);
+    % first figure - by condition (unmasked at end)
+    figure()
+    plot(cc,pp,'*')
+    ax = gca;
+    
+    ylabel('Mean score (k)'); xlabel('Exposure duration (ms)');
+    xlim([0 8]); ylim([0 4]);
+    
+    % second figure by exposure duration (unmasked intermixed)
+    figure()
+    plot(tt,pp,'*')
+    ylabel('Mean score (k)'); xlabel('Exposure duration (ms)');
+    xlim([0 250]); ylim([0 4]);
+    
+    tvaResults.(sprintf('%s', participantID)).vSTM = pp;
+end
 
 %% Exporting
