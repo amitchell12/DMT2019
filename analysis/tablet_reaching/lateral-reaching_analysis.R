adb8e663ee91fa3058dfe416faa9cbd1ae3a0b85
amitchell12 <agm26@st-andrews.ac.uk>
2019-10-21 17:41:10 +0100

    Lateral reaching analysis
    Extracting relevant files and giving logical names

--- analysis/tablet_reaching/lateral-reaching_analysis.R
+++ analysis/tablet_reaching/lateral-reaching_analysis.R
@@ -11,26 +11,38 @@ setwd(dataPath)
 #variable information
 nParticipants = 2 #for testing
 #nParticipants = 1:24 #for analysis
+nTasks = 1:6 #total number of tasks - free + peripheral reaching, visual detection (non-dominant, dominant)
 
 #make complete list of RESULTS files (trial information)
 for (x in nParticipants){
-  ppID = sprintf("10%s", nParticipants[x])
+  ppID = sprintf("10%s", x)
   ppPath = (file.path(dataPath, ppID))
-  resfiles = (path=ppPath, full.names=TRUE, recursive = FALSE, 
+  resfiles = list.files(path=ppPath, full.names=TRUE, recursive = FALSE, 
                              include.dirs = FALSE, pattern = "*.csv")
+  
+  for (t in nTasks){
+    task_name = switch(t, "peripheral_l", "peripheral_r","free_l", "free_r",
+                       "detection_l", "detection_r")
+    tmp <- read.csv(resfiles[t])
+    file_name = sprintf("%s_%s", ppID, task_name)
+    assign(file_name, tmp) #assigning csv file logical name, yay!
+  }
+  
 }
   
 
 #make empty dataframe to stack up trial information
-CLF <- read.csv(text = "avg_rt,count_committed,count_valid_trial,datetime,eye_move,fix_x,land_x,land_y,reach_duration,response_time_target_mouse_response,sanity_check,subject_nr,subject_parity,targ_x,targ_y,target_onset,targets_file,time_touch_offset,touch_x")
+#CLF <- read.csv(text = "avg_rt,count_committed,count_valid_trial,datetime,eye_move,fix_x,land_x,land_y,reach_duration,response_time_target_mouse_response,sanity_check,subject_nr,subject_parity,targ_x,targ_y,target_onset,targets_file,time_touch_offset,touch_x")
 
 
 #stack them all up
-for(x in resfiles){
-  tmp <- read.csv(x)
+#for(x in resfiles){
+#  tmp <- read.csv(x)
+#}
+  
   tmp$subject_nr <- as.numeric(substr(x, 44, 46))
   CLF <- rbind(CLF, tmp)
-}
+
 
 #organise
 CLF <- CLF[CLF$targ_x > 0, c(5,7,8,12,14,15)]
