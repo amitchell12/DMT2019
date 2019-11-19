d385987181f1468d1d9cb486fce80854d3d75424
Alex <alexandra.mitchell@ed.ac.uk>
2019-11-19 17:21:21 +0000

    Re-naming

--- analysis/radial_reaching/radial-reaching_analysis.R
+++ analysis/radial_reaching/radial-reaching_analysis.R
@@ -1,45 +1,51 @@
 library(readr)
 
-mypath <- getwd()
+dataPath <- 'S:/groups/DMT/data'
+anaPath <- 'S:/groups/DMT/analysis/radial_reaching'
+setwd(dataPath)
 
-files_201 <- list.files(path=mypath, pattern = "*.TRJ",
-                        full.names = TRUE, recursive = TRUE)
+files <- list.files(path=dataPath, pattern = "*.TRJ", full.names = TRUE, recursive = TRUE)
+idfiles <- list.files(path=dataPath, pattern = "*.dmt", full.names = TRUE, recursive = TRUE)
+
 
 optodat <- read.csv(text="SIDE,VIEW,TRIAL,RT,MT,PS,TPS,mx,my")
 caldat <- read.csv(text="cx,cy")
 
+caldat$POSITION <- c(-100,-200,-300,-400,100,200,300,400)
+iddat <- read.csv(text = 'PPT,SIDE,VIEW,TRIAL,POSITION,EYE_MOVE,FILENUM')
+
+# reading in all idfiles and compiling
+for(i in idfiles){
+  tmp <- read.csv(i, sep='\t')[, c(1:6,9)]
+  iddat <- rbind(iddat, tmp)
+}
+
 row_x <- 1
 row_c <- 1
 
-for(i in files_201){
-  tmp <- as.numeric(t(as.vector(read_tsv(i, col_names = FALSE)[1:14, 7])))
-  SIDE <- substr(i,nchar(i)- 10,nchar(i)- 10)
-  VIEW <- substr(i,nchar(i)- 8,nchar(i)- 8)
-  TRIAL <- as.numeric(substr(i,nchar(i)- 6,nchar(i)- 4))
-  if(VIEW=="F"){VIEW <- "FREE"}else{VIEW<-"PERIPH"}
-  if(SIDE == "L"){
-  optodat[row_x, 1:9] <- c("LEFT", VIEW, TRIAL, tmp[c(1:4,9:10)])
-  row_x <- row_x+1
-  } else if (SIDE == "R") {
-    optodat[row_x, 1:9] <- c("RIGHT", VIEW, TRIAL, tmp[c(1:4,9:10)])
-    row_x <- row_x+1
-  } else {
-    caldat[row_c, 1:2] <- tmp[10:11]
-    row_c <- row_c+1
+## unsure whether this is needed - do not need to compile TRJ files by side/condition
+# needed only for patients 202 and 201, as it stands this doesn't work - figure it out later
+for(i in files){
+  if (isTRUE(substr(basename(files), 4, 4)=="_")){
+    tmp <- as.numeric(t(as.vector(read_tsv(i, col_names = FALSE)[1:14, 7])))
+    SIDE <- substr(i,nchar(i)- 10,nchar(i)- 10)
+    VIEW <- substr(i,nchar(i)- 8,nchar(i)- 8)
+    TRIAL <- as.numeric(substr(i,nchar(i)- 6,nchar(i)- 4))
+    if(VIEW=="F"){VIEW <- "FREE"}else{VIEW<-"PERIPH"}
+    if(SIDE == "L"){
+      optodat[row_x, 1:9] <- c("LEFT", VIEW, TRIAL, tmp[c(1:4,9:10)])
+      row_x <- row_x+1
+    } else if (SIDE == "R") {
+      optodat[row_x, 1:9] <- c("RIGHT", VIEW, TRIAL, tmp[c(1:4,9:10)])
+      row_x <- row_x+1
+    } else {
+      caldat[row_c, 1:2] <- tmp[10:11]
+      row_c <- row_c+1
+    }
   }
+ 
 }
 
-caldat$POSITION <- c(-100,-200,-300,-400,100,200,300,400)
-
-idfiles <- list.files(path=mypath, pattern = "*.dmt",
-          full.names = TRUE, recursive = TRUE)
-
-iddat <- read.csv(text="PPT,SIDE,VIEW,TRIAL,POSITION,EYE_MOVE,DATE,TIME")
-
-for(i in idfiles){
-  tmp <- read.csv(i)
-  iddat <- rbind(iddat, tmp)
-}
 
 optodat$TRIAL <- as.numeric(optodat$TRIAL)
 optodat$SIDE <- factor(optodat$SIDE)
