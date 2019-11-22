4b04494f422bc1508971f9f1668ed7bb6e37b100
Alex <alexandra.mitchell@ed.ac.uk>
2019-11-22 13:56:30 +0000

    Update radial-reaching_analysis.R
    Plotting data for radial reaching task :) all done for now!

--- analysis/radial_reaching/radial-reaching_analysis.R
+++ analysis/radial_reaching/radial-reaching_analysis.R
@@ -112,14 +112,16 @@ write.csv(res, "radial-reaching_compiled.csv", row.names = FALSE)
 res_medians <- aggregate(AEdeg ~ POSITION*VIEW*SIDE*PPT*GRP, median, data = res)
 colnames(res_medians)[colnames(res_medians)=='AEdeg'] <- 'AEmed'
 res_means <- aggregate(AEmed ~ VIEW*SIDE*PPT*GRP, mean, data = res_medians)
+colnames(res_means)[colnames(res_means)=='AEmed'] <- 'AEmean'
 
 # changing levels of PMI for plotting
 res_means$VIEW <- factor(res_means$VIEW) #changing so only 2 levels recorded
 res_means$SIDE <- factor(res_means$SIDE, levels = c('LEFT', 'RIGHT'))
 levels(res_means$SIDE) <- c('Left', 'Right')
 levels(res_means$GRP) <- c('Control', 'AD') #changing group name from 1 = control, 2 = AD
-levels(res_means$VIEW) <- c('Peripheral', 'Free')
+levels(res_means$VIEW) <- c('Free', 'Peripheral')
 res_means$PPT <- substr(res_means$PPT, 4, 6)
+
 # casting by task
 PMIdata <- dcast(res_means, PPT+GRP+SIDE ~ VIEW)
 PMIdata$PMI <- PMIdata$Peripheral - PMIdata$Free
@@ -127,12 +129,33 @@ write.csv(PMIdata, 'radial-reaching_PMI.csv', row.names = FALSE)
 
 
 ##### plots #####
+# mean plot 
+ggplot(res_means, aes(x = SIDE, y = AEmean, colour = GRP)) +
+  geom_point(shape = 1, size = 2) +
+  geom_line(aes(group = PPT), size = 0.5, alpha = .5) +
+  facet_grid(cols = vars(VIEW), rows = vars(GRP)) + ylim(-.5,8) +
+  labs(x = 'Side', y = 'Mean AE (deg)', element_text(size = 12)) +
+  scale_colour_manual(values = c('black', 'grey50')) +
+  theme_bw() + theme(legend.position = 'none', text = element_text(size = 10),
+                     strip.text.x = element_text(size = 8)) -> meansPlot
 
+ggsave('allmeans_plot.png', plot = last_plot(), device = NULL, dpi = 300, 
+       scale = 1, path = anaPath)
 
+# PMI plot
+ggplot(PMIdata, aes(x = SIDE, y = PMI, colour = GRP), position = position_dodge(.2)) + 
+  geom_point(shape = 1, size = 1.5, stroke = .8) +
+  geom_line(aes(group = PPT), alpha = .5, size = .5) +
+  scale_colour_manual(values = c('grey40', 'grey40')) +
+  stat_summary(aes(y = PMI, group = 1), fun.y = mean, colour = "black", 
+               geom = 'point', shape = 3, stroke = 1, size = 2, group = 1) +
+  ylim(-.5,8) + labs(x = 'Side', y = 'PMI (deg)', element_text(size = 12)) +
+  facet_wrap(~GRP) +
+  theme_bw() + theme(legend.position = 'none', text = element_text(size = 10),
+                     strip.text.x = element_text(size = 8)) -> PMIplot
 
-
-
-
+ggsave('allPMI_plot.png', plot = last_plot(), device = NULL, dpi = 300, 
+       scale = 1, path = anaPath)
 
 
 
