a9f67acbd6b6f06a61024b90a2076e7c682ca3f1
Alex <alexandra.mitchell@ed.ac.uk>
2019-11-22 13:48:02 +0000

    Update radial-reaching_analysis.R
    Calculating descriptives and PMI for all PP

--- analysis/radial_reaching/radial-reaching_analysis.R
+++ analysis/radial_reaching/radial-reaching_analysis.R
@@ -1,5 +1,6 @@
 library(readr)
 library(ggplot2)
+library(rshape2)
 
 #on mac
 #anaPath <- '/Users/alexandramitchell/Documents/EDB_PostDoc/DMT2019/analysis/radial_reaching'
@@ -12,6 +13,14 @@ setwd(dataPath)
 files <- list.files(path=dataPath, pattern = "*.TRJ", full.names = TRUE, recursive = TRUE)
 idfiles <- list.files(path=dataPath, pattern = "*.dmt", full.names = TRUE, recursive = TRUE)
 
+##### variables #####
+visAngle <- function(size, distance){
+  # this function calculates visual angle
+  # size and distance must be in the same units
+  Rad = 2*atan(size/(2*distance))
+  Ang = Rad*(180/pi)
+  return(Ang)
+}
 
 optodat <- read.csv(text="RT,MT,PS,TPS,PAX,TPAX,mx,my,COUNT")
 iddat <- read.csv(text = 'PPT,SIDE,VIEW,TRIAL,POSITION,EYE_MOVE,FILENUM')
@@ -45,7 +54,7 @@ res <- res[order(res$COUNT, decreasing = FALSE), ] #ordering by count (so all in
 res <- res[!res$PPT == 'DMT300', ]
 # saving combined file
 setwd(anaPath)
-write.csv(res, "radial-reaching_compiled.csv", row.names = FALSE)
+write.csv(res, "radial-reaching_allData.csv", row.names = FALSE)
 
 ##### calibration data #####
 # calibration data-frame
@@ -61,15 +70,25 @@ res <- res[!res$VIEW =='CAL', ] #data-frame without cal trials, not needed
 res <- merge(res, caldat, by = c('PPT','POSITION'), all = TRUE) #SO HAPPY THIS FUNCT EXISTS
 res[res == -32768] <- NA
 
-# plotting to get a look at data
-ggplot(res) + geom_point(aes(x = calx, y = caly, colour = POSITION), shape = 3) +
-  geom_point(aes(x = mx, y = my, colour = POSITION)) +
-  facet_wrap(~PPT*VIEW)
-
 ##### data analysis #####
 # subtracting cal from reach endpoint
 res$LANDx <- res$mx - res$calx
 res$LANDy <- res$my - res$caly
+
+# counting eye-move and removing eye-move + void
+nEye_move <- aggregate(res$EYE_MOVE == '1', by=list(subject_nr = res$PPT), FUN=sum)
+nVoid <- aggregate(res$EYE_MOVE == '1', by=list(subject_nr = res$PPT), FUN=sum)
+# removing
+res <- res[res$EYE_MOVE == 0, c(1:6,9:10,12:13,17:20)]
+
+
+# plotting to get a look at data
+
+ggplot(res) + geom_point(aes(x = calx, y = caly, colour = POSITION), shape = 3) +
+  geom_point(aes(x = mx, y = my, colour = POSITION)) +
+  facet_wrap(~PPT*VIEW)
+ggsave('radial-reach_Err.png', plot = last_plot(), device = NULL, 
+       path = anaPath, scale = 1, width = 15, height = 10, units = 'in')
 
 # plotting position vs land_y
 ggplot(res) + geom_point(aes(x = POSITION, y = LANDy, colour = VIEW)) +
@@ -77,6 +96,45 @@ ggplot(res) + geom_point(aes(x = POSITION, y = LANDy, colour = VIEW)) +
 
 ggsave('radial-reach_yErr.png', plot = last_plot(), device = NULL, 
        path = anaPath, scale = 1, width = 15, height = 10, units = 'in')
+
+# tranforming to degrees
+res$LANDx_deg <- visAngle(size= res$LANDx, distance = 500) #using visual angle function above
+res$LANDy_deg <- visAngle(size= res$LANDy, distance = 500)
+# absolute error
+res$AE <- sqrt(res$LANDx^2 + res$LANDy^2) #mm
+res$AEdeg <- sqrt(res$LANDx_deg^2 + res$LANDy_deg^2) #deg
+res$GRP <- factor(substr(res$PPT, 4, 4))
+
+#save compiled data-set
+write.csv(res, "radial-reaching_compiled.csv", row.names = FALSE)
+
+# summary data
+res_medians <- aggregate(AEdeg ~ POSITION*VIEW*SIDE*PPT*GRP, median, data = res)
+colnames(res_medians)[colnames(res_medians)=='AEdeg'] <- 'AEmed'
+res_means <- aggregate(AEmed ~ VIEW*SIDE*PPT*GRP, mean, data = res_medians)
+
+# changing levels of PMI for plotting
+res_means$VIEW <- factor(res_means$VIEW) #changing so only 2 levels recorded
+res_means$SIDE <- factor(res_means$SIDE, levels = c('LEFT', 'RIGHT'))
+levels(res_means$SIDE) <- c('Left', 'Right')
+levels(res_means$GRP) <- c('Control', 'AD') #changing group name from 1 = control, 2 = AD
+levels(res_means$VIEW) <- c('Peripheral', 'Free')
+res_means$PPT <- substr(res_means$PPT, 4, 6)
+# casting by task
+PMIdata <- dcast(res_means, PPT+GRP+SIDE ~ VIEW)
+PMIdata$PMI <- PMIdata$Peripheral - PMIdata$Free
+write.csv(PMIdata, 'radial-reaching_PMI.csv', row.names = FALSE)
+
+
+##### plots #####
+
+
+
+
+
+
+
+
 
 
 
