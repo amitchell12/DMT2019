d93f2294ed1c83936d92f8974be09da47401b161
amitchell12 <agm26@st-andrews.ac.uk>
2019-12-09 18:56:16 +0000

    TVA analysis
    Creating file with all key PP info
    Plotting info for tomorrow

--- analysis/TVA/tva_fits-analysis.r
+++ analysis/TVA/tva_fits-analysis.r
@@ -3,12 +3,21 @@
 ## AG.Mitchell 06.12.2019 ##
 
 ##### file info #####
-#creating folder to sae data in
-dataPath <- 'S:/groups/DMT/data/formatted_TVA/'
-fitPath <- ("S:/groups/DMT/analysis/TVA/fits/") # Enter path to data
-anaPath <- "S:/groups/DMT/analysis/TVA/"
+#on mac
+dataPath <- '/Users/alexandramitchell/Documents/EDB_PostDoc/DMT2019/data/formatted_TVA'
+fitPath <- '/Users/alexandramitchell/Documents/EDB_PostDoc/DMT2019/analysis/TVA/fits/'
+anaPath <- '/Users/alexandramitchell/Documents/EDB_PostDoc/DMT2019/analysis/TVA/'
+# on pc
+#dataPath <- 'S:/groups/DMT/data/formatted_TVA/'
+#fitPath <- ("S:/groups/DMT/analysis/TVA/fits/") # Enter path to data
+#anaPath <- "S:/groups/DMT/analysis/TVA/"
+
 # Enter directory to save converted files to
 setwd(fitPath)
+
+##### functions #####
+library(stringr)
+library(ggplot2)
 
 # list all files in working directory
 txt_filelist <- list.files(fitPath, ".txt")
@@ -27,11 +36,71 @@ for (i in 1:length(txt_filelist)) {
 }
  
 ##### extracting key data #####
-# first fing t0 for each participant
-### can't get this to work - try and fix
-dat_filelist <- list.files(dataPath, ".dat")
-for (i in dat_filelist){
-  if (substr(basename(dat_filelist[i]), -4, -4)){
-    
+# create data-frame
+tva_values <- read.csv(text = 'SUB,COND,K,C,T0')
+tva_filelist <- dir(fitPath, recursive = TRUE, full.names = FALSE, pattern = '.csv')
+# key values from .csv files - ALL COND FIRST
+for (file in tva_filelist){
+  if (isTRUE(str_sub(basename(file), -10, -10)=='r')){
+    tmp <- read.csv(file)[, c(2,15,28)]
+    tmp$COND <- 'all'
+    tmp$SUB <- substr(basename(file), 8, 10)
+    tmp <- tmp[, c(5,4,1:3)]
+    tva_values <- rbind(tva_values, tmp)
+  }
+  
+}
+
+tva_values$GRP <- factor(substr(tva_values$SUB, 1, 1))
+levels(tva_values$GRP) <- c('Control', 'Patient')
+tva_values$SUB <- factor(tva_values$SUB)
+names(tva_values)[5] <- 't0'
+
+#save tva-values
+setwd(anaPath)
+write.csv(tva_values, 'tva_data.csv', row.names = FALSE)
+
+##### plotting #####
+# processing speed
+ggplot(tva_values, aes(x = GRP, y = C)) + 
+  geom_jitter(aes(colour = GRP), position = position_jitter(0.1)) + 
+  scale_color_manual(values = c('black', 'grey50')) +
+  labs(title = 'Processing speed (C)', x = 'Group', y = 'C (item/s)', 
+                              element_text(size = 12)) +
+  theme_bw() + theme(legend.position = 'none', text = element_text(size = 12))
+
+ggsave('processing-speed.png', plot = last_plot(), device = NULL, dpi = 300, 
+       scale = 1, path = anaPath)
+
+# vSTM
+ggplot(tva_values, aes(x = GRP, y = K)) + 
+  geom_jitter(aes(colour = GRP), position = position_jitter(0.1)) + 
+  scale_color_manual(values = c('black', 'grey50')) +
+  labs(title = 'vSTM', x = 'Group', y = 'K (number of items)', 
+       element_text(size = 12)) +
+  theme_bw() + theme(legend.position = 'none', text = element_text(size = 12))
+
+ggsave('vSTM.png', plot = last_plot(), device = NULL, dpi = 300, 
+       scale = 1, path = anaPath)
+
+# t0
+ggplot(tva_values, aes(x = GRP, y = t0)) + 
+  geom_jitter(aes(colour = GRP), position = position_jitter(0.1)) + 
+  scale_color_manual(values = c('black', 'grey50')) +
+  labs(title = 'Perceptual threshold (t0)', x = 'Group', y = 't0 (ms)', 
+       element_text(size = 12)) +
+  theme_bw() + theme(legend.position = 'none', text = element_text(size = 12))
+
+ggsave('perceptual-thresh.png', plot = last_plot(), device = NULL, dpi = 300, 
+       scale = 1, path = anaPath)
+
+# find t0 for each participant (from other data set, don't need for now)
+setwd(dataPath)
+dat_filelist <- dir(dataPath, recursive = TRUE, full.names = TRUE, pattern = '.dat')
+for (file in dat_filelist){
+  if (isTRUE(str_sub(basename(file), -5, -5)=="r")){
+    tmp <- read.table(file, header=FALSE, sep="\t")
+    timing <- tmp[,2] #column of timing variables
+    t <- min(timing, na.rm = TRUE) #getting minimum values
   }
 }
