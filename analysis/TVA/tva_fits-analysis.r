176e169f49ad7661cc7672885f84a2ca750be254
Alex <alexandra.mitchell@ed.ac.uk>
2019-12-09 14:34:31 +0000

    Update tva_fits-analysis.r
    Extracting t0 (beginning to)

--- analysis/TVA/tva_fits-analysis.r
+++ analysis/TVA/tva_fits-analysis.r
@@ -4,23 +4,34 @@
 
 ##### file info #####
 #creating folder to sae data in
-dataPath <- ("S:/groups/DMT/analysis/TVA/fits/") # Enter path to data
+dataPath <- 'S:/groups/DMT/data/formatted_TVA/'
+fitPath <- ("S:/groups/DMT/analysis/TVA/fits/") # Enter path to data
 anaPath <- "S:/groups/DMT/analysis/TVA/"
 # Enter directory to save converted files to
-setwd(dataPath)
+setwd(fitPath)
 
 # list all files in working directory
-txt_filelist <- list.files(dataPath, ".txt")
+txt_filelist <- list.files(fitPath, ".txt")
 
+# converting to .csv files
 for (i in 1:length(txt_filelist)) {
   FILE = read.table(file=txt_filelist[i], header = TRUE, sep = '\t')
   
   write.table(
     FILE,
-    file=paste0(dataPath,
+    file=paste0(fitPath,
                 sub(".txt","",txt_filelist[i]),".csv"),
     row.names=FALSE,
     quote = FALSE,
     sep=",")
 }
  
+##### extracting key data #####
+# first fing t0 for each participant
+### can't get this to work - try and fix
+dat_filelist <- list.files(dataPath, ".dat")
+for (i in dat_filelist){
+  if (substr(basename(dat_filelist[i]), -4, -4)){
+    
+  }
+}
