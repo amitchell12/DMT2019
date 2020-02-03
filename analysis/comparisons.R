86d3542354ba98ac02f9029aab34219c7a87f294
Alex <alexandra.mitchell@ed.ac.uk>
2020-02-03 13:09:57 +0000

    Update comparisons.R
    Adding TVA data to correlations

--- analysis/comparisons.R
+++ analysis/comparisons.R
@@ -17,8 +17,12 @@ latData <- read.csv('lateral-reaching_PMI.csv')
 radPath <- 'S:/groups/DMT/analysis/radial_reaching'
 setwd(radPath)
 radData <- read.csv('radial-reaching_PMI.csv')
+# TVA path
+TVApath <- 'S:/groups/DMT/analysis/TVA/'
+setwd(TVApath)
+TVAData <- read.csv('TVA_values.csv')
 
-# making sure both matrices are the same
+# making sure all matrices are the same
 names(latData)[1] <- 'PPT'
 names(latData)[2] <- 'GRP'
 names(latData)[3] <- 'SIDE'
@@ -30,12 +34,15 @@ latData <- latData[c(1,2,3,5,4,6)] #reorgansing to fit radData
 latData$TASK <- 'Lateral'
 radData$TASK <- 'Radial'
 # merge
-dat <- rbind(latData, radData)
-corrdat <- dcast(dat, PPT+GRP+SIDE ~ TASK, value.var = 'PMI')
+dat_side <- rbind(latData, radData)
+corrDat_side <- dcast(dat_side, PPT+GRP+SIDE ~ TASK, value.var = 'PMI')
+dat <- aggregate(PMI ~ TASK * GRP * PPT, mean, data = dat_side) 
+corrDat <- dcast(dat, PPT+GRP ~ TASK, value.var = 'PMI')
 
-ggscatter(corrdat, x = 'Lateral', y = 'Radial', add = 'reg.line', conf.int = TRUE,
+ggscatter(corrDat_side, x = 'Lateral', y = 'Radial', add = 'reg.line', conf.int = TRUE,
           cor.coef = TRUE, cor.method = 'pearson') + 
-  facet_wrap(~GRP) + ylab('Radial reaching PMI (deg)') + xlab('Lateral reaching PMI (deg)')
+  facet_grid(cols = vars(SIDE), rows = vars(GRP)) + 
+  ylab('Radial reaching PMI (deg)') + xlab('Lateral reaching PMI (deg)')
 
 ggsave('task_correlations_pearson.png', plot = last_plot(), device = NULL, dpi = 300, 
        scale = 1, path = anaPath)
@@ -47,12 +54,16 @@ names(latData)[6] <- 'PMI_lat'
 names(radData)[4] <- 'Free_rad'
 names(radData)[5] <- 'Perip_rad'
 names(radData)[6] <- 'PMI_rad'
-allDat <- merge(latData, radData, by = c('PPT', 'GRP', 'SIDE'), all = FALSE)
+allDat <- merge(latData, radData, by = c('PPT', 'GRP', 'SIDE'), all = TRUE)
 allDat <- allDat[c(1:5, 8:9, 6, 10) ]
 
+###### TVA data ######
+# ranaming to match reaching data-frame
+names(TVAData)[2] <- 'PPT'
+TVAData <- TVAData[c(2:7)]
+# merge with allDat
+allDat <- merge(allDat, TVAData, by = c('PPT', 'GRP'), all = TRUE)
+
 setwd(anaPath)
-write.csv(allDat, 'lat-rad-reaching_res.csv', row.names = FALSE)
-
-###### TVA data #####
-
+write.csv(allDat, 'all-task_results.csv', row.names = FALSE)
 
