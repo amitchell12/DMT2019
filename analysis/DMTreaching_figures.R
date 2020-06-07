f613a29b075a9f96d30acaaebdfd9a4becb7b653
amitchell12 <agm26@st-andrews.ac.uk>
2020-06-07 13:16:19 +0100

    Create DMTreaching_figures.R
    Making script to create figures for publication

---
+++ analysis/DMTreaching_figures.R
@@ -0,0 +1,18 @@
+## FIGURES FOR PUBLICATION ## 
+library(readr)
+library(ggplot2)
+
+#set working directory to where data is
+# on desktop mac
+latPath <- '/Users/Alex/Documents/DMT/analysis/lateral_reaching'
+radPath <- '/Users/Alex/Documents/DMT/analysis/radial_reaching'
+#on pc
+#latPath <- 'S:/groups/DMT/analysis/lateral_reaching'
+#radPath <- 'S:/groups/DMT/analysis/radial_reaching'
+
+
+###### LATERAL REACHING FIGURES ######
+setwd(latPath)
+
+
+###### RADIAL REACHING FIGURES ######
