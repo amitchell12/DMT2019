39034a354cf117c42967d0231bbcee32249a4d9e
Alex <alexandra.mitchell@ed.ac.uk>
2020-08-07 10:00:03 +0100

    Update modellingTVA.m
    Adapting and improving script - save all in one txt file

--- analysis/TVA/modellingTVA.m
+++ analysis/TVA/modellingTVA.m
@@ -11,31 +11,30 @@
 %dataDir = '/Users/Alex/Documents/DMT/data/formatted_TVA';
 %anaDir = '/Users/Alex/Documents/DMT/analysis/TVA/fits';
 % on desktop
-dataDir = 'S:\groups\DMT\data\formatted_TVA';
+dataDir = 'S:\groups\DMT\data\TVA\formatted_TVA';
 anaDir = 'S:\groups\DMT\analysis\TVA\all';
 cd(dataDir)
 files = dir(fullfile(dataDir, 'subject*.dat'));
 
 %% TVA file prep
 for i = 1:length(files)
-    cd(dataDir)
-    participantID = files(i).name(1:10);
-    filename = files(i).name(end-8:end-4);
+   
     filecheck = tvacheckdatafile(files(i).name) %leave open
     
     % load data
     % need to include the 'STD' [5 6] for masked conditions
     tvadata{i} = tvaloader(files(i).name, 'STD', [6 7]);
-    % tva report on data
-    %leave open to view
+end
     
     %% Fitting model
-    [theta{i}, tvamodel{i}, tvadata{i}, df{i}] = tvafit(tvadata, [], 'FREE');
+for i = 1:length(files)
+     participantID = files(i).name(1:10);
+    [theta{i}, tvamodel{i}, tvadata{i}, df{i}] = tvafit(tvadata{i}, [], 'FREE');
     %see the fitted values
-    tvareport(tvadata, tvamodel, theta);
+    tvareport(tvadata{i}, tvamodel{i}, theta{i});
     
     %% Plotting
-    [tt, oo, pp, cc] = tvaplot(tvadata, tvamodel, theta);
+    [tt, oo, pp, cc] = tvaplot(tvadata{i}, tvamodel{i}, theta{i});
     % first figure - by condition (unmasked at end)
     figure()
     plot(cc,pp,'*')
@@ -46,7 +45,7 @@ for i = 1:length(files)
     xlim([0 8]); ylim([0 4]);
     
     cd([anaDir filesep 'fit-plots'])
-    png_name = sprintf('%s_%s.png', participantID, filename);
+    png_name = sprintf('%s_%s.png', participantID);
     saveas(gcf, png_name)
     
     % second figure by exposure duration (unmasked intermixed)
