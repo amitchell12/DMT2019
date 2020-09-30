6f7028e444dfd363f4327fa322b7baeb36410748
amitchell12 <agm26@st-andrews.ac.uk>
2020-09-30 16:58:32 +0100

    Update lateral-reaching_exploratory.R

--- analysis/lateral_reaching/lateral-reaching_exploratory.R
+++ analysis/lateral_reaching/lateral-reaching_exploratory.R
@@ -12,11 +12,11 @@ library(psychReport)
 
 ###### GETTING DATA #######
 #on mac
-#anaPath <- '/Users/alexandramitchell/Documents/EDB_PostDoc/DMT2019/analysis/lateral_reaching'
-#dataPath <- '/Users/alexandramitchell/Documents/EDB_PostDoc/DMT2019/data/'
+anaPath <- '/Users/alexandramitchell/Documents/EDB_PostDoc/DMT2019/analysis/lateral_reaching'
+dataPath <- '/Users/alexandramitchell/Documents/EDB_PostDoc/DMT2019/data/'
 # on desktop mac
-anaPath <- '/Users/Alex/Documents/DMT/analysis/lateral_reaching'
-dataPath <- '/Users/Alex/Documents/DMT/data'
+#anaPath <- '/Users/Alex/Documents/DMT/analysis/lateral_reaching'
+#dataPath <- '/Users/Alex/Documents/DMT/data'
 setwd(anaPath)
 
 res <- read.csv('lateral-reaching_compiled.csv')
