26c4e048bdd26d44d78f4d5602bd85fdf0908b8d
Alex <alexandra.mitchell@ed.ac.uk>
2021-05-25 13:42:34 +0100

    Update radial-reaching-PScompiler.R
    Extracting same info from UEA data

--- reaching/analysis/compilers/radial-reaching-PScompiler.R
+++ reaching/analysis/compilers/radial-reaching-PScompiler.R
@@ -96,12 +96,190 @@ cal <- cal[-c(1) ,]
 cal <- cal[, c(1:3,6,14,15)]
 
 # merging data-frames with new cal-value
-res <- merge(res, cal, by = c('PPT','POSITION','SIDE'), all = TRUE) #SO HAPPY THIS FUNCT EXISTS
-res[res == -32768] <- NA
-res <- res[, c(1:3,5:13,15,16)]
+resUOE <- merge(res, cal, by = c('PPT','POSITION','SIDE'), all = TRUE) #SO HAPPY THIS FUNCT EXISTS
+resUOE[resUOE == -32768] <- NA
+resUOE <- resUOE[, c(1:3,5:13,15,16)]
 
 
 ###### UEA DATA ######
+# work computer
+dataPath <- 'S:/groups/DMT/norwich_movement_data'
+setwd(dataPath)
+
+files <- list.files(path=dataPath, pattern = "*.TRJ", full.names = TRUE, recursive = TRUE)
+idfiles <- list.files(path=dataPath, pattern = "*.csv", full.names = TRUE, recursive = TRUE)
+
+# data-frame structures
+optodat <- read.csv(text="PS,TPS,FILE,COUNT")
+caldat <- read.csv(text="RT,MT,PS,TPS,PAX,TPAX,calx,caly,FILE")
+iddat <- read.csv(text = 'POSITION,DELAY,RT,TO,RESP,FILE,TRIAL')
+
+# reading in all idfiles for trial information, and compiling
+# excluding practice or voided blocks
+for(i in idfiles){
+  if (isTRUE(substr(i, nchar(i)-5, nchar(i)-4) == "et")){
+    tmp <- read.csv(i, header = FALSE)[, c(1:5)]
+    colnames(tmp) <- NULL #removing column names
+    tmp$FILE <- basename(i)
+    tmp$TRIAL <- 1:length(tmp$FILE)
+    iddat <- rbind(iddat, tmp) #data-frame of trial information
+  }
+}
+# adding key trial information
+iddat$PPT <- substr(iddat$FILE, 1, 3)
+iddat$SIDE <- substr(iddat$FILE, (nchar(iddat$FILE)-11), (nchar(iddat$FILE)-10))
+iddat$VIEW <- substr(iddat$FILE, 14, 14)
+
+# re-ordering and naming columns
+names(iddat)[1] <- 'POSITION'
+names(iddat)[2] <- 'DELAY'
+names(iddat)[3] <- 'RTID'
+names(iddat)[4] <- 'TIMEOUT'
+names(iddat)[5] <- 'RESP'
+# reorder so key info in first few columns
+iddat <- iddat[, c(8:10,1:5,7)]
+
+row_x <-1
+# now for movement (TRJ files), reading in and compiling
+for(file in files) {
+  # get movement (reaaching data-files)
+  if (isTRUE(substr(file, nchar(file)-9, nchar(file)-8) == "et")){
+    FILE <- basename(file)
+    tmp <- c(as.numeric(unlist(read_tsv(file, col_names = FALSE)[1:14, 7])), FILE)
+    TPSrow <- (as.numeric(tmp[4])/5.58) +1
+    TPSrow <- round(TPSrow)
+    tmp1 <- as.numeric(t(as.vector(read_tsv(file, col_names = FALSE)[TPSrow, 1:2])))
+    
+    optodat[row_x, 1:2] <- tmp1
+    optodat[row_x, 3:5] <- tmp[c(3:4,15)]
+    optodat$COUNT[row_x] <- row_x #counter
+    row_x <- row_x + 1
+  }
+}
+
+# extracting key features from FILE in optodat in the same manner as iddat - for merging
+optodat$PPT <- substr(optodat$FILE, 5, 7)
+optodat$SIDE <- substr(optodat$FILE, (nchar(optodat$FILE)-15), (nchar(optodat$FILE)-14))
+optodat$VIEW <- substr(optodat$FILE, 18, 18)
+optodat$TRIAL <- substr(optodat$FILE, (nchar(optodat$FILE)-5), (nchar(optodat$FILE)-4))
+# removing '0' that pre-fix digits
+optodat$TRIAL <- gsub("(^|[^0-9])0+", "\\1", optodat$TRIAL, perl = TRUE)
+
+row_x <- 1
+cal_x <- 1
+# get calibration files
+for(file in files) {
+  # get movement (reaaching data-files)
+  if (isTRUE(substr(file, nchar(file)-13, nchar(file)-12) == "st")){
+    FILE <- basename(file)
+    tmp <- c(as.numeric(unlist(read_tsv(file, col_names = FALSE)[1:14, 7])), FILE)
+    
+    caldat[row_x, 1:9] <- tmp[c(1:6,9:10,15)]
+    caldat$COUNT[row_x] <- cal_x #counter for each calib trial
+    row_x <- row_x + 1
+    if(isTRUE(cal_x >= 10)){
+      cal_x <- (cal_x+1)-10
+    }
+    else{
+      cal_x <- cal_x + 1
+    }
+  }
+}
+
+# extracting key features from FILE in optodat in the same manner as iddat - for merging
+optodat$PPT <- substr(optodat$FILE, 5, 7)
+optodat$SIDE <- substr(optodat$FILE, (nchar(optodat$FILE)-15), (nchar(optodat$FILE)-14))
+optodat$VIEW <- substr(optodat$FILE, 18, 18)
+optodat$TRIAL <- substr(optodat$FILE, (nchar(optodat$FILE)-5), (nchar(optodat$FILE)-4))
+# removing '0' that pre-fix digits
+optodat$TRIAL <- gsub("(^|[^0-9])0+", "\\1", optodat$TRIAL, perl = TRUE)
+
+## back to caldat
+# getting 'position' and 'ppt' data for caldat
+caldat$PPT <- substr(caldat$FILE, 5, 7)
+
+# saving button locations for calculating amplitude and angular error
+button_left <- caldat[caldat$COUNT == 5 ,]
+button_right <- caldat[caldat$COUNT == 6 ,]
+# get side
+button_left$SIDE <- 'Left'
+button_right$SIDE <- 'Right'
+buttons <- rbind(button_left, button_right)
+buttons$calx <- as.numeric(buttons$calx)
+buttons$caly <- as.numeric(buttons$caly)
+# one erroneous trial, correct
+for (i in 1:length(buttons$PPT)){
+  if (isTRUE(buttons$calx[i] < -100)){
+    buttons$calx[i] = -30
+    buttons$caly[i] = 39
+  }
+}
+# rename columns
+names(buttons)[7] <- 'sX'
+names(buttons)[8] <- 'sY'
+# remove unnecessary data
+buttons <- buttons[, c(7,8,11,12)]
+
+# position information- from counter and calx
+# first remove 6+7 (button presses)
+caldat <- subset(caldat, caldat$COUNT !=5 & caldat$COUNT !=6)
+# flag position - should just be list 1:4
+caldat$POSITION <- c(1:4)
+# flag side based on -ve and +ve signed
+caldat$SIDE <- factor(caldat$calx < 0, labels = c('Right','Left'))
+# extract key variables
+caldat <- caldat[, c(11:13,7:8)]
+## merge button data to caldat
+caldat <- merge(caldat, buttons, by = c('SIDE','PPT'))
+
+## missing calibration data - load this
+cal_fits <- read.csv(text='TRIAL,rX,rY,FILE')
+for(i in idfiles){
+  if (isTRUE(substr(basename(i), 11, 14) == 'fits')){
+    tmp <- read.csv(i, header = FALSE)[, c(1,2,4)]
+    tmp$FILE <- basename(i)
+    cal_fits <- rbind(cal_fits, tmp) #data-frame of trial information
+  }
+}
+
+# re-arrage to fit other calibration data, then bind and order
+colnames(cal_fits)[colnames(cal_fits) == 'V2'] <- 'calx' #renaming for merging
+colnames(cal_fits)[colnames(cal_fits) == 'V4'] <- 'caly'
+cal_fits$PPT <- substr(cal_fits$FILE, 1, 3)
+cal_fits$POSITION <- c(1:4)
+cal_fits$SIDE <- factor(cal_fits$calx < 0, labels = c('Right','Left'))
+## add missing button data to cal_fits
+# estimate start position from the mean of the other values - for missing data
+mean_startX <- summarySEwithin(data = buttons, measurevar = 'sX', withinvars = 'SIDE')
+mean_startY <- summarySEwithin(data = buttons, measurevar = 'sY', withinvars = 'SIDE')
+# adding mean fitted data to missing data-set
+for (i in 1:length(cal_fits$PPT)){
+  if (isTRUE(cal_fits$SIDE[i] == 'Left')){
+    cal_fits$sX[i] <- mean_startX$sX[1]
+    cal_fits$sY[i] <- mean_startY$sY[1]
+  }
+  else if (isTRUE(cal_fits$SIDE[i] == 'Right')){
+    cal_fits$sX[i] <- mean_startX$sX[2]
+    cal_fits$sY[i] <- mean_startY$sY[2]
+  }
+}
+
+# merge 
+cal_fits <- cal_fits[, c(5:7,2,3,8,9)]
+caldat <- rbind(caldat,cal_fits)
+
+### MERGE
+### IDDAT AND OPTODAT TRIALS DO NOT ALIGN - THIS IS AN ISSUE, MAYBE SPEAK TO THE OTHERS ABOUT THIS?
+# combining id data and optotrak data
+res <- merge(iddat, optodat, by = c('PPT', 'SIDE', 'VIEW', 'TRIAL'))
+res <- res[order(res$COUNT, decreasing = FALSE), ] #ordering by count (so all in order)
+# renaming conditions to be more sensible :)
+res$SIDE <- factor(res$SIDE, labels = c('Left', 'Right'))
+res$VIEW <- factor(res$VIEW, labels = c('Free', 'Peripheral'))
+# remove file column
+res <- res[, c(1:17,19)]
+
+
 
 
 ##### PREP FOR DATA ANALYSIS #####
